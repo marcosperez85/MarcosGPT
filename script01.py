5fa16ab8f1edc9ad80111a010132e52dfa3fbe0e
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-02-05 12:40:55 -0300

    Feature: agrego menu de navegacion

--- script01.py
+++ script01.py
@@ -10,35 +10,57 @@ session = boto3.Session(profile_name = "AdministratorAccess-376129873205")
 bedrock_runtime = session.client('bedrock-runtime', region_name='us-east-1')
 
 print("\n*** Bienvenido a Marcos GPT ***")
-prompt = input("\n¿En qué puedo ayudarte?\n\n")
 
-# ### Generation Configuration
+def menuPrincipal():
+    while True:
 
-kwargs = {
-    "modelId": "amazon.titan-text-lite-v1",
-    "contentType": "application/json",
-    "accept": "*/*",
-    "body": json.dumps(
-        {
-            "inputText": prompt,
-            "textGenerationConfig": {
-                "maxTokenCount": 100,
-                "temperature": 0,
-                "topP": 0.9
+        print("\nMenú principal")
+        print("1) Realizar una pregunta")
+        print("2) Salir del programa")
+
+        opcionElegida = input("\nSeleccione una opción: ")
+
+        if opcionElegida == "1" :
+                
+            prompt = input("\n¿En qué puedo ayudarte?\n\n>> ")
+
+            # ### Generation Configuration
+            kwargs = {
+                "modelId": "amazon.titan-text-lite-v1",
+                "contentType": "application/json",
+                "accept": "*/*",
+                "body": json.dumps(
+                    {
+                        "inputText": prompt,
+                        "textGenerationConfig": {
+                            "maxTokenCount": 200,
+                            "temperature": 0,
+                            "topP": 0.9
+                        }
+                    }
+                )
             }
-        }
-    )
-}
 
-# Invoco al modelo de la misma forma que se hizo con los ejemplos de antes sólo que ahora el prompt es el contenido
-# de un archivo de texto.
-# Al usar un "temperature" de 0, logro mayor consistencia en las respuestas sin que el LLM sea demasiado creativo.
-response = bedrock_runtime.invoke_model(**kwargs)
+            print("\nGenerando respuesta...")
 
-response_body = json.loads(response.get('body').read())
-generation = response_body['results'][0]['outputText']
+            response = bedrock_runtime.invoke_model(**kwargs)
 
-print(f"{generation}\n")
+            response_body = json.loads(response.get('body').read())
+            generation = response_body['results'][0]['outputText']
+
+            print(f"\n{generation}\n")
+
+        elif opcionElegida == "2":
+            print("\nMuchas gracias por usar este servicio\n")
+            break
+        else:
+            print("\nOpción no valida. Intente de nuevo.")
+
+# Llamamos a la función para iniciar el programa
+menuPrincipal()
+
+
+
 
 
 
